d7c95f4a3f9425ff1a5da5bd41baf34f060b0f37
nongaloza <diomedicalsa@gmail.com>
2016-10-09 16:18:38 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   end
   resources :comments
   resources :posts
-  root 'index#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
